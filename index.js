ddceef0b342bf390e76b011ba56ed694db41a7f4
unknown <morzmamun@gmail.com>
2023-11-09 15:50:13 +0600

    final check up done

--- index.js
+++ index.js
@@ -1,22 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
-const cookieParser = require("cookie-parser");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(
-  cors({
-    origin: ["https://dream-job-finder.web.app",'https://dream-job-finder.firebaseapp.com' ],
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json());
-app.use(cookieParser());
-
 // mongoDB code
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ttjbmkn.mongodb.net/?retryWrites=true&w=majority`;
@@ -31,21 +23,21 @@ const client = new MongoClient(uri, {
 });
 
 //  token verify middleware
-const tokenVerify = async (req, res, next) => {
-  const token = req.cookies.token;
-  if (!token) {
-    return res.status(401).send({ message: "unauthorized" });
-  }
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if (err) {
-      return res.status(401).send({ message: "unauthorized access" });
-    }
+// const tokenVerify = async (req, res, next) => {
+//   const token = req.cookies.token;
+//   if (!token) {
+//     return res.status(401).send({ message: "unauthorized" });
+//   }
+//   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+//     if (err) {
+//       return res.status(401).send({ message: "unauthorized access" });
+//     }
 
-    req.user = decoded;
+//     req.user = decoded;
 
-    next();
-  });
-};
+//     next();
+//   });
+// };
 
 async function run() {
   try {
@@ -59,29 +51,30 @@ async function run() {
 
     // JWT API User authorization
 
-    app.post("/jwt", async (req, res) => {
-      const user = req.body;
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: "3h",
-      });
-      res
-        .cookie("token", token, {
-          httpOnly: true,
-          secure: true,
-          sameSite: "none",
-        })
-        .send({ success: true });
-    });
+    // app.post("/jwt", async (req, res) => {
+    //   const user = req.body;
+    //   const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+    //     expiresIn: "3h",
+    //   });
+    //   res
+    //     .cookie("token", token, {
+    //       httpOnly: true,
+    //       secure: true,
+    //       sameSite: "none",
+    //     })
+    //     .send({ success: true });
+    // });
 
     // clear token api
-    app.post("/logout", async (req, res) => {
-      const user = req.body;
-      res.clearCookie("token", { maxAge: 0 }).send({ success: true });
-    });
+    // app.post("/logout", async (req, res) => {
+    //   const user = req.body;
+    //   res.clearCookie("token", { maxAge: 0 }).send({ success: true });
+    // });
 
+    
     // bidProject related Api
 
-    app.get("/bidprojects", tokenVerify, async (req, res) => {
+    app.get("/bidprojects", async (req, res) => {
       let query = {};
       if (req.query?.email) {
         query = { userEmail: req.query.email };
@@ -90,14 +83,14 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/bidprojects/:email", tokenVerify, async (req, res) => {
+    app.get("/bidprojects/:email", async (req, res) => {
       const email = req.params.email;
       const filter = { buyerEmail: email };
       const result = await bidProjectCollection.find(filter).toArray();
       res.send(result);
     });
 
-    app.patch("/bidprojects/:email/:id", tokenVerify, async (req, res) => {
+    app.patch("/bidprojects/:email/:id", async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const updateBidRequest = req.body;
@@ -118,12 +111,12 @@ async function run() {
     });
 
     // add jobs related api
-    app.get("/addjobs", tokenVerify, async (req, res) => {
+    app.get("/addjobs", async (req, res) => {
       const result = await addJobCollection.find().toArray();
       res.send(result);
     });
 
-    app.get("/addjobs/:category", tokenVerify, async (req, res) => {
+    app.get("/addjobs/:category", async (req, res) => {
       const category = req.params.category;
       let regex = new RegExp(["^", category, "$"].join(""), "i");
       const filter = { category: regex };
@@ -131,14 +124,14 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/:category/:id", tokenVerify, async (req, res) => {
+    app.get("/:category/:id", async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const result = await addJobCollection.find(filter).toArray();
       res.send(result);
     });
 
-    app.get("/:email", tokenVerify, async (req, res) => {
+    app.get("/:email", async (req, res) => {
       const email = req.params.email;
       console.log(email);
       const filter = { email: email };
@@ -152,7 +145,7 @@ async function run() {
       res.send(result);
     });
 
-    app.put("/addjobs/:id", tokenVerify, async (req, res) => {
+    app.put("/addjobs/:id", async (req, res) => {
       const updateJob = req.body;
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
@@ -171,7 +164,7 @@ async function run() {
       res.send(result);
     });
 
-    app.delete("/addjobs/:id", tokenVerify, async (req, res) => {
+    app.delete("/addjobs/:id", async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const result = await addJobCollection.deleteOne(filter);
